d8a88545e71a06c4d6bf4ce420fa48549839e6a6
stevie2codes <stevie2codes@gmail.com>
2019-11-15 15:36:51 -0600

    add JS file and got current date to display in the jumbotron

---
+++ script.js
@@ -0,0 +1,106 @@
+// The app should display standard business hours (9 a.m. to 5 p.m.).
+//  Each time slot should represent one hour and contain the following:
+
+// * The time
+
+// * A field to hold user input
+
+// * A save button
+
+// Clicking on the save button will store the time and user input in `localStorage`.
+
+
+
+$(document).ready(function(){
+
+    let currentDate = moment().format('LLLL');
+    console.log(currentDate);
+
+    $("#currentDay").text(currentDate);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
