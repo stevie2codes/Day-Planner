d3c83476bbb8f27f4cbaf9d1e4513fb2632a8a84
stevie2codes <stevie2codes@gmail.com>
2019-11-19 10:22:13 -0600

    fixed timeblocks to show proper color had to add children to the timeblock

--- script.js
+++ script.js
@@ -20,45 +20,46 @@ $(document).ready(function () {
     //getting current hour in normal time
     let now = moment();
     let hour = now.hour();
-    let formatHour = now.format('h');
+    let formatHour = now.format('ha');
     console.log(formatHour);
 
-
     let timeBlocks = $(".time-block");
+
     for (let i = 0; i < timeBlocks.length; i++) {
         let blockTime = $(timeBlocks[i]);
         let hourId = blockTime.attr("id");
-
+        console.log(hourId);
+        
+        
         if (hourId === formatHour) {
-            $(blockTime).addClass("present");
-        } 
-            if (moment(hourId).isBefore(formatHour)) {
-                $(blockTime).addClass("past");
-            } 
-                if (moment(hourId).isAfter(formatHour)) {
-                    $(blockTime).addClass("future");
-                }
+            $(blockTime).children(".row").addClass("present");
+        }
+        if (moment(hourId,"ha").isBefore()) {
+            $(blockTime).children(".row").addClass("past");
+        } else
+            if (moment(hourId,"ha").isAfter()) {
+                $(blockTime).children(".row").addClass("future");
+            }
     }
-
     let textArea = $("#text-area"); //comment input
-    let timeBlock = $(".time-block"); //comment placement
+
     let saveBtn = $("#saveBtn"); //save button
 
-    
+
 
     let storedValue = JSON.parse(localStorage.getItem('text'));
-        if(storedValue) {
-            textArea.html(storedValue);
-            
-        }
-        $(".saveBtn").on("click",function(event){
-            event.preventDefault();
-            localStorage.setItem('text', JSON.stringify(textArea.val()));
-             
-        });
+    if (storedValue) {
+        textArea.html(storedValue);
+
+    }
+    $(".saveBtn").on("click", function (event) {
+        event.preventDefault();
+        localStorage.setItem('text', JSON.stringify(textArea.val()));
+
+    });
 
 
-      
+
 
 
 
